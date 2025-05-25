2450b65701ea561e6a43e666674b877167664d35
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-05-25 18:35:19 +0600

    Model class created

---
+++ app/src/main/java/com/arsoft/themovieapppaging/model/Movie.java
@@ -0,0 +1,54 @@
+package com.arsoft.themovieapppaging.model;
+
+import androidx.annotation.Nullable;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Movie {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+
+    @SerializedName("poster_path")
+    @Expose
+    private String posterPath;
+
+    @SerializedName("vote_average")
+    @Expose
+    private double voteAverage;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public double isVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+
+    @Override
+    public boolean equals(@Nullable Object o) {
+        if (o == null) return false;
+        if (o == this) return true;
+        return false;
+    }
+}
